2e7191838e3c894b409f9c29914be527a4ae83f3
Hlynur Stefánsson <hlynurs15@ru.is>
2016-10-19 18:39:46 +0000

    Snake gets longer when it eats food

--- game_functions.py
+++ game_functions.py
@@ -4,14 +4,17 @@ from random import randint
 
 def update_screen(screen, settings, snake, food):
     """Update everything on screen and then draw the screen."""
-    screen.fill((0, 0, 0))
-
-    if snake_ate_food(snake, food):
+    screen.fill(settings.black)
+    ate = snake_ate_food(snake, food)
+    if ate:
         create_food(screen, settings, food)
     food.blitme()
 
-    snake.update()
+    snake.update(ate)
     snake.blitme()
+
+
+
     check_edges(settings, snake)
 
     pygame.display.update()
@@ -28,26 +31,27 @@ def check_events(snake):
 
 def check_keydown_events(event, snake):
     """Check for keypresses and respond to them."""
+    # TODO: Find a way to prevent player from pressing down + left/right quickly to go backwards.
     if event.key == pygame.K_ESCAPE:
         quit_pygame()
-    if event.key == pygame.K_UP:
+    if event.key == pygame.K_UP and not snake.moving_down:
         snake.set_direction(0, -1)
-    if event.key == pygame.K_DOWN:
+    elif event.key == pygame.K_DOWN and not snake.moving_up:
         snake.set_direction(0, 1)
-    if event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT and not snake.moving_right:
         snake.set_direction(-1, 0)
-    if event.key == pygame.K_RIGHT:
+    elif event.key == pygame.K_RIGHT and not snake.moving_left:
         snake.set_direction(1, 0)
 
 
 def quit_pygame():
-    """Quits pygame and python"""
+    """Quits pygame and python."""
     pygame.quit()
     quit()
 
 
 def check_edges(settings, snake):
-    """Check if snake is touching the edges."""
+    """Check if snake is beyond the edges."""
     if ((snake.rect.x + settings.scale > settings.width) or
        (snake.rect.x < 0) or
        (snake.rect.y + settings.scale > settings.height) or
@@ -73,6 +77,7 @@ def snake_ate_food(snake, food):
     """Returns true if the snake has eaten the food."""
     if (snake.head.rect.x == food.rect.x and
         snake.head.rect.y == food.rect.y):
+        #snake.add_tail()
         return True
     else:
         return False
--- snake.py
+++ snake.py
@@ -11,13 +11,14 @@ class Snake():
         self.tail = []
         self.initialize_snake()
         self.scale = settings.scale
-        self.x_dir = 0
-        self.y_dir = 0
+
 
     def initialize_snake(self):
         """Initialize snake object."""
         self.x = 400
         self.y = 300
+        self.x_dir = 0
+        self.y_dir = 0
         self.head = Block(self.screen, self.settings, self.settings.white,
                           self.x, self.y)
         self.rect = self.head.rect
@@ -35,21 +36,17 @@ class Snake():
 
 
     def set_direction(self, x, y):
-        if (x == -1 and not self.moving_right or
-            x == 1  and not self.moving_left or
-            y == -1 and not self.moving_down or
-            y == 1  and not self.moving_up):
-            self.x_dir = x
-            self.y_dir = y
-            self.reset_movement_flags()
-            if x == 1:
-                self.moving_right = True
-            elif x == -1:
-                self.moving_left = True
-            elif y == 1:
-                self.moving_down = True
-            elif y == -1:
-                self.moving_up = True
+        self.x_dir = x
+        self.y_dir = y
+        self.reset_movement_flags()
+        if x == 1:
+            self.moving_right = True
+        elif x == -1:
+            self.moving_left = True
+        elif y == 1:
+            self.moving_down = True
+        elif y == -1:
+            self.moving_up = True
 
 
     def reset_movement_flags(self):
@@ -58,9 +55,19 @@ class Snake():
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
-        
 
-    def update(self):
+
+    def update(self, ate):
         """Update the position of the snake."""
+        x = self.rect.x
+        y = self.rect.y
         self.rect.x = self.rect.x + self.scale * self.x_dir
         self.rect.y = self.rect.y + self.scale * self.y_dir
+        if not ate:
+            if len(self.tail) > 0:
+                del self.tail[0]
+                self.tail.append(Block(self.screen, self.settings,
+                                           self.settings.white, x, y))
+        elif ate:
+            self.tail.append(Block(self.screen, self.settings,
+                                       self.settings.white, x, y))
